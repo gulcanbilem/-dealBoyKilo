1fd713e9d49d91fbd21c2ec3bc22fdfa2764454f
gulcanbilem <113993110+gulcanbilem@users.noreply.github.com>
2023-01-01 18:21:22 +0300

    Add files via upload

---
+++ IdealBoyKilo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdealBoyKilo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cinsiyet;
+            int kilo, boy;
+            Console.WriteLine("Cinsiyetinizi Yazın=");
+            cinsiyet = Console.ReadLine();
+            Console.WriteLine("Kilonuzu giriniz = ");
+            kilo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Boyunuzu giriniz = ");
+            boy = Convert.ToInt32(Console.ReadLine());
+
+            boy = boy % 100;
+            if (cinsiyet == "kadın" && kilo > 45.5 + 2.3 * boy) ;
+            {
+
+                Console.WriteLine("Kilo vermeniz gerek");
+            }
+            Console.ReadKey();
+            if (cinsiyet == "kadın" && kilo == 45.5 + 2.3 * boy) ;
+            { 
+
+                Console.WriteLine("Kilonuz ideal");
+            }
+            Console.ReadKey();
+            if (cinsiyet == "kadın" && kilo < 45.5 + 2.3 * boy) ;
+            { 
+            Console.WriteLine("Kilo almanız gerek.");
+
+                } Console.ReadKey();
+            if (cinsiyet == "erkek" && kilo > 50 + 2.3 * boy) ;
+            {
+
+                Console.WriteLine("Kilo vermeniz gerek");
+            }
+            Console.ReadKey();
+
+            if (cinsiyet == "erkek" && kilo == 50 + 2.3 * boy) ;
+            {
+
+                Console.WriteLine("Kilonuz ideal");
+            }
+            Console.ReadKey();
+            if (cinsiyet == "erkek" && kilo < 50 + 2.3 * boy) ;
+            {
+                Console.WriteLine("Kilo almanız gerek.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
